b0befe63608b4cf03d5d0e26bdabe023581fe3e9
Vladimir Grubor <grubor@gmail.com>
2012-08-27 23:39:28 +0200

    clean rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VladoTest::Application.config.secret_token = '1ecd59b0365ebbfe7a7925c4940ded49031457d2a2cf62fe481c10268a9f9c934385adf34b347d5d578ddc9362766a785c60b78b7af60428e5d0952664d62cd9'
